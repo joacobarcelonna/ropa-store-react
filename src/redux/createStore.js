c9ed1ada014712410536b4f882cb970ce5defb03
Joaco Barcelonna <joacobarcelonna@gmail.com>
2021-02-08 14:41:09 -0300

    mantener compra carrito con redux-persist

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import createSagaMiddle from 'redux-saga'
-
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga'
@@ -15,4 +15,10 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
 
-export default store;
+export const persistor = persistStore(store);
+// eslint-disable-next-line 
+export default {
+  store,
+  persistor
+};
+
